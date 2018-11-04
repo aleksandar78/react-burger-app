dc67cb0811fabe4bdebd7e5f7fc6bd9027341c4e
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2018-11-04 23:18:18 +0100

    Implement less and more button action in build control comp

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './BuildControls.css';
 import BuildControl from "./BuildControl/BuildControl";
 
-const controlls = [
+const controls = [
   { label: 'Salad', type: 'salad' },
   { label: 'Bacon', type: 'bacon' },
   { label: 'Cheese', type: 'cheese' },
@@ -12,7 +12,14 @@ const controlls = [
 const buildControls = props => {
   return (
     <div className={classes.BuildControls}>
-      {controlls.map(ctrl => (<BuildControl key={ctrl.label} label={ctrl.label} />))}
+      {controls.map(ctrl => (
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+        />
+      ))}
     </div>
   );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux';
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
+const INGREDIENT_PRICE = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
+
 export class BurgerBuilder extends Component{
   
   state = {
@@ -11,14 +18,39 @@ export class BurgerBuilder extends Component{
       bacon: 0,
       cheese: 0,
       meat: 0,
-    }
+    },
+    totalPrice: 4
   };
+  
+  cloneState = () => {
+    return {...this.state};
+  };
+
+  addIngredientHandler = (type) => {
+    const currentState = this.cloneState();
+    currentState.ingredients[type] += 1;
+    currentState.totalPrice += INGREDIENT_PRICE[type];
+    this.setState(currentState);
+  };
+  
+  removeIngredientHandler = (type) => {
+    const currentState = this.cloneState();
+    if (currentState.ingredients[type] > 0 ) {
+      currentState.ingredients[type] -= 1;
+      currentState.totalPrice -= INGREDIENT_PRICE[type];
+    }
+    this.setState(currentState);
+  };
+  
   
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+        />
       </Aux>
     );
   }
