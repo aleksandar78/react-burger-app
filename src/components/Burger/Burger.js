df1459f1783b24bde554b350c4565e2f733cb8e9
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2018-11-04 13:32:01 +0100

    Add default message when ingredient list is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import classes from './Burger.css';
-import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
-
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
   
   const ingredients = Object.keys(props.ingredients);
-  const burgerIngredients = ingredients.map(ing => [...Array(props.ingredients[ing])]
-    .map((_, i) => <BurgerIngredient key={ing + i} type={ing}/>));
+  let burgerIngredients = ingredients.map(ing => [...Array(props.ingredients[ing])]
+    .map((_, i) => <BurgerIngredient key={ing + i} type={ing}/>))
+    .reduce((acc, el) => acc.concat(el), []);
+  
+  if (burgerIngredients.length === 0) {
+    burgerIngredients = <p>Please start adding some ingredients</p>;
+  }
   
   return (
     <div className={classes.Burger}>
